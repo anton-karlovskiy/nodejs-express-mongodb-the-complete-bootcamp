df22950514e1bbbb1c86dd96cd06b241d4efc088
Anton K. <antonkarlovskiy@outlook.com>
2025-11-13 22:49:10 -0500

    chore: clean up

--- projects/4-natours/app.js
+++ projects/4-natours/app.js
@@ -13,7 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-const bookingRouter = requrie('./routes/bookingRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
